a68a6d686b4fceb4eec120604955e50608bcbb6a
soohykeee <gur1028@skuniv.ac.kr>
2023-10-27 12:43:06 +0900

    Initial commit

---
+++ src/main/java/kr/co/imguru/ImGuruApplication.java
@@ -0,0 +1,13 @@
+package kr.co.imguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImGuruApplication.class, args);
+    }
+
+}
