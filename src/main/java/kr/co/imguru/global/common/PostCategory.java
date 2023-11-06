70be74295d4ed41a06034c8193f5cfc0572a1f6a
soohykeee <gur1028@skuniv.ac.kr>
2023-11-06 12:23:03 +0900

    feat: 생성일자, 수정일자, 삭제여부를 포함한 BaseEntity + 각종 enum 클래스 생성

---
+++ src/main/java/kr/co/imguru/global/common/PostCategory.java
@@ -0,0 +1,28 @@
+package kr.co.imguru.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum PostCategory {
+
+    NOTICE("공지사항"),
+
+    QNA("질문"),
+
+    INFO("정보공유"),
+
+    FREE("자유게시판");
+
+    String postCategory;
+
+//    public static PostCategory of(String postCategory) {
+//        return Arrays.stream(PostCategory.values())
+//                .filter(type -> type.toString().equalsIgnoreCase(postCategory))
+//                .findAny().orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_POST_CATEGORY_NOT_FOUND));
+//    }
+
+}
---
+++ src/main/java/kr/co/imguru/global/common/Role.java
@@ -0,0 +1,29 @@
+package kr.co.imguru.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+
+    ROLE_ADMIN("관리자"),
+
+    ROLE_MANAGER("매니저"),
+
+    ROLE_GURU("도사"),
+
+    ROLE_USER("회원");
+
+    String userRole;
+
+//    public static Role of(String userRole) {
+//
+//        return Arrays.stream(Role.values())
+//                .filter(role -> role.toString().equalsIgnoreCase(userRole))
+//                .findAny().orElseThrow(() -> new NotFoundException(ResponseStatus.FAIL_MEMBER_ROLE_NOT_FOUND));
+//    }
+
+}
