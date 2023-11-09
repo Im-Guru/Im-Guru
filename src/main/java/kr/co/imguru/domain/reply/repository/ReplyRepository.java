b83c9a6943790d8dd4db535bafe20b530202f054
soohykeee <gur1028@skuniv.ac.kr>
2023-11-09 12:04:19 +0900

    refactor: Reply - querydsl 사용할 SearchRepository 생성 후, fetch join이 필요한 경우만 적용

--- src/main/java/kr/co/imguru/domain/reply/repository/ReplyRepository.java
+++ src/main/java/kr/co/imguru/domain/reply/repository/ReplyRepository.java
@@ -2,6 +2,7 @@ package kr.co.imguru.domain.reply.repository;
 
 import kr.co.imguru.domain.reply.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +12,6 @@ import java.util.Optional;
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     Optional<Reply> findByIdAndIsDeleteFalse(Long id);
-
-    List<Reply> findAllByPost_IdAndIsDeleteFalse(Long postId);
-
-    List<Reply> findAllByMember_NicknameAndIsDeleteFalse(String memberNickname);
 
     List<Reply> findAllByIsDeleteFalse();
 
---
+++ src/main/java/kr/co/imguru/domain/reply/repository/ReplySearchRepository.java
@@ -0,0 +1,52 @@
+package kr.co.imguru.domain.reply.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.imguru.domain.member.entity.QMember;
+import kr.co.imguru.domain.post.entity.QPost;
+import kr.co.imguru.domain.reply.entity.QReply;
+import kr.co.imguru.domain.reply.entity.Reply;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ReplySearchRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    private QReply reply = QReply.reply;
+
+    private QMember member = QMember.member;
+
+    private QPost post = QPost.post;
+
+    public List<Reply> findRepliesByMemberNickname(String memberNickname) {
+        return queryFactory
+                .selectFrom(reply)
+                .join(member)
+                .on(reply.member.id.eq(member.id))
+                .fetchJoin()
+                .where(
+                        reply.isDelete.eq(Boolean.FALSE),
+                        reply.member.nickname.eq(memberNickname)
+                )
+                .fetch();
+    }
+
+    public List<Reply> findRepliesByPostId(Long postId) {
+        return queryFactory
+                .selectFrom(reply)
+                .join(post)
+                .on(reply.post.id.eq(post.id))
+                .fetchJoin()
+                .where(
+                        reply.isDelete.eq(Boolean.FALSE),
+                        reply.post.id.eq(postId)
+                )
+                .fetch();
+    }
+
+
+}
