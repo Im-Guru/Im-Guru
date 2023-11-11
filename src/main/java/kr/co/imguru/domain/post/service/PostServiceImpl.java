6f56c2a2790fa2e438e586ab4b2decb22393c0ea
soohykeee <gur1028@skuniv.ac.kr>
2023-11-11 16:51:44 +0900

    refactor: 상세 조회 시, 조회 수 올라가는 로직 추가

--- src/main/java/kr/co/imguru/domain/post/service/PostServiceImpl.java
+++ src/main/java/kr/co/imguru/domain/post/service/PostServiceImpl.java
@@ -20,10 +20,15 @@ import kr.co.imguru.global.exception.IllegalArgumentException;
 import kr.co.imguru.global.exception.NotFoundException;
 import kr.co.imguru.global.model.ResponseStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -38,6 +43,8 @@ public class PostServiceImpl implements PostService {
     private final PostSearchRepository postSearchRepository;
 
     private final LikePostSearchRepository likePostSearchRepository;
+
+    private final RedisTemplate<String, Object> redisTemplate; // RedisTemplate 주입
 
     @Override
     @Transactional
@@ -57,6 +64,8 @@ public class PostServiceImpl implements PostService {
         Optional<Post> post = postRepository.findByIdAndIsDeleteFalse(postId);
 
         isPost(post);
+
+        updateCntToRedis(postId, "views");
 
         return toReadDto(post.get());
     }
@@ -151,6 +160,65 @@ public class PostServiceImpl implements PostService {
         post.get().changeDeleteAt();
 
         postRepository.save(post.get());
+    }
+
+
+    /*
+    게시글 상세조회 요청 시, 해당 postId에 해당하는 viewCnt를 +1 해준 값을 Redis에 저장
+     */
+    @Override
+    @Transactional
+    public void updateCntToRedis(Long postId, String hashKey) {
+        HashOperations<String, String, Object> hashOperations = redisTemplate.opsForHash();
+
+        String key = "postId::" + postId;
+
+        if (hashOperations.get(key, hashKey) == null) {
+            if (hashKey.equals("views")) {
+                hashOperations.put(key, hashKey, postRepository.findByIdAndIsDeleteFalse(postId).get().getViewCnt());
+            }
+            hashOperations.increment(key, hashKey, 1L);
+            System.out.println("hashOperations.get is null ---- " + hashOperations.get(key, hashKey));
+        } else {
+            hashOperations.increment(key, hashKey, 1L);
+            System.out.println("hashOperations.get is not null ---- " + hashOperations.get(key, hashKey));
+        }
+    }
+
+    /*
+   Redis에 기록된 정보들을 DB에 업데이트를 진행하면서 데이터의 일관성을 유지하고, Redis의 저장된 정보들을 초기화
+   Spring Scheduled를 사용하여 일정 시간마다 실행이 되도록 설정
+    */
+    @Transactional
+    @Scheduled(fixedDelay = 1000L * 180L) // 180초
+    public void deleteCntToRedis() {
+        String viewKey = "views";
+        Set<String> redisKey = redisTemplate.keys("postId*");
+        Iterator<String> it = redisKey.iterator();
+
+        while (it.hasNext()) {
+            String data = it.next();
+            Long postId = Long.parseLong(data.split("::")[1]);
+
+            if (redisTemplate.opsForHash().get(data, viewKey) == null) {
+                break;
+            } else {
+                Long viewCnt = Long.parseLong(String.valueOf(redisTemplate.opsForHash().get(data, viewKey)));
+                addViewCntFromRedis(postId, viewCnt);
+                redisTemplate.opsForHash().delete(data, viewKey);
+            }
+        }
+        System.out.println("Update Complete From Redis");
+    }
+
+    private void addViewCntFromRedis(Long postId, Long viewCnt) {
+        Optional<Post> post = postRepository.findByIdAndIsDeleteFalse(postId);
+
+        if (!post.isEmpty()) {
+            post.get().addViewCnt(viewCnt);
+
+            postRepository.save(post.get());
+        }
     }
 
     private void isMember(Optional<Member> member) {
