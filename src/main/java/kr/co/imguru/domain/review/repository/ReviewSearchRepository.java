3cb717e14d45b80e189234655063a59cf510c3f9
soohykeee <gur1028@skuniv.ac.kr>
2023-11-12 16:16:47 +0900

    refactor: Like 파일 구조 수정 반영, Transactional 추가

--- src/main/java/kr/co/imguru/domain/review/repository/ReviewSearchRepository.java
+++ src/main/java/kr/co/imguru/domain/review/repository/ReviewSearchRepository.java
@@ -1,7 +1,7 @@
 package kr.co.imguru.domain.review.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import kr.co.imguru.domain.like.QLikeReview;
+import kr.co.imguru.domain.like.entity.QLikeReview;
 import kr.co.imguru.domain.member.entity.QMember;
 import kr.co.imguru.domain.review.entity.QReview;
 import kr.co.imguru.domain.review.entity.Review;
