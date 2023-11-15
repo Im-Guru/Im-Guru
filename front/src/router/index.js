fc505a9d5e9c746a4f9a85fb208b6fc08aa5de30
soohykeee <gur1028@skuniv.ac.kr>
2023-11-15 11:29:41 +0900

    refactor: 만들어준 페이지들 사용하기 위해서 router에 추가

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PageHome from '@/views/PageHome.vue'
+import store from "@/vuex/store";
+
+import PageHome from '@/views/common/PageHome.vue'
+import JoinGuru from "@/views/common/JoinGuru.vue";
+import JoinMember from "@/views/common/JoinMember.vue";
+import LoginMember from "@/views/common/LoginMember.vue";
+
 import PostList from "@/views/post/PostList.vue";
 
+import MessageList from "@/views/message/MessageList.vue";
+
+
+const requireAuth = () => (from, to, next) => {
+    const token = localStorage.getItem('user_token')
+    if (token) {
+        store.state.isLogin = true
+        return next()
+    } // isLogin === true면 페이지 이동
+    next('/login') // isLogin === false면 다시 로그인 화면으로 이동
+}
+
 const routes = [
+    // common
     {
         path: '/',
         name: 'PageHome',
@@ -14,12 +33,41 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
+        component: () => import(/* webpackChunkName: "about" */ '../views/common/PageAbout.vue')
     },
+    {
+        path: '/join/member',
+        name: 'JoinMember',
+        component: JoinMember
+    },
+    {
+        path: '/join/guru',
+        name: 'JoinGuru',
+        component: JoinGuru
+    },
+    {
+        path: '/login',
+        name: 'LoginMember',
+        component: LoginMember
+    },
+
+
+
+    // Post
     {
         path: '/post/list',
         name: 'PostList',
         component: PostList
+    },
+
+
+
+    // Message
+    {
+        path: '/message/list',
+        name: 'MessageList',
+        component: MessageList,
+        beforeEnter: requireAuth()
     }
 ]
 
