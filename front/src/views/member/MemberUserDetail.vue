7bd382f286a3c10bf4780ee7e0a21ed870ea1814
soohykeee <gur1028@skuniv.ac.kr>
2023-11-24 11:55:57 +0900

    feat: member의 마이페이지 front + member의 프로필 페이지 front

---
+++ front/src/views/member/MemberUserDetail.vue
@@ -0,0 +1,356 @@
+<template>
+  <div>
+    <div class="container mt-5">
+      <h3><strong>회원 정보 수정</strong></h3><br><br>
+      <div class="row justify-content-center">
+        <div class="col-md-5">
+          <b-card class="mb-5">
+            <b-form @submit.prevent="onSubmit" class="form-width mt-3" style="margin: auto;">
+              <div class="mb-4">
+                <label for="input-1" class="form-label"><strong>이메일</strong></label>
+                <b-input-group class="form-height">
+                  <b-form-input v-model="form.email" type="email" id="input-1" placeholder="이메일을 입력해주세요" ref="emailInput" disabled></b-form-input>
+                  <b-form-input v-model="form.email" type="email" id="input-1" placeholder="이메일을 입력해주세요" ref="emailInput" hidden></b-form-input>
+                </b-input-group>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-2" class="for정-label text-md-right"><strong>비밀번호</strong></label>
+                <b-form-input class="form-height" v-model="form.password" type="password" id="input-3"
+                              ref="passwordInput"
+                              placeholder="비밀번호를 입력해주세요" required></b-form-input>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-2-1" class="form-label text-md-right"><strong>비밀번호 확인</strong></label>
+                <b-form-input class="form-height" v-model="form.confirmPassword" type="password" id="input-3-1"
+                              ref="confirmPasswordInput"
+                              placeholder="비밀번호 확인을 입력해주세요" required></b-form-input>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-3" class="form-label text-md-right"><strong>이름</strong></label>
+                <b-form-input class="form-height" v-model="form.name" type="text" id="input-2" placeholder="이름을 입력해주세요"
+                              ref="nameInput" required></b-form-input>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-4" class="form-label text-md-right"><strong>닉네임</strong></label>
+                <b-input-group class="form-height">
+                  <b-form-input v-model="form.nickname" type="text" id="input-4" placeholder="닉네임을 입력해주세요" ref="nicknameInput" disabled></b-form-input>
+                  <b-form-input v-model="form.nickname" type="text" id="input-4" placeholder="닉네임을 입력해주세요" ref="nicknameInput" hidden></b-form-input>
+                </b-input-group>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-5" class="form-label text-md-right"><strong>전화번호</strong></label>
+                <b-input-group class="form-height">
+                  <b-form-input v-model="form.telephone" type="tel" id="input-5" ref="telephoneInput" placeholder="전화번호를 입력해주세요" disabled></b-form-input>
+                  <b-form-input v-model="form.telephone" type="tel" id="input-5" ref="telephoneInput" placeholder="전화번호를 입력해주세요" hidden></b-form-input>
+                </b-input-group>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-2" class="form-label text-md-right"><strong>직업</strong></label>
+                <b-form-input class="form-height" v-model="form.job" type="text" id="input-2" placeholder="직업을 입력해주세요"
+                              ref="jobInput" required></b-form-input>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-6" class="form-label text-md-right"><strong>주소</strong></label>
+                <b-input-group>
+                  <b-form-input v-model="form.zoneCode" type="text" id="input-6-1" placeholder="우편번호" ref="zoneCodeInput"
+                                readonly></b-form-input>
+                  <b-button id="postcode" @click="openPostcode">검색</b-button>
+                </b-input-group>
+                <div class="row mt-2">
+                  <div class="col-md-12">
+                    <b-form-input v-model="form.roadAddress" type="text" id="input-6-2" placeholder="주소"
+                                  ref="roadAddressInput" readonly></b-form-input>
+                  </div>
+                </div>
+                <div class="row mt-2">
+                  <div class="col-md-12">
+                    <b-form-input v-model="form.detailAddress" type="text" id="input-6-3"
+                                  ref="detailAddressInput" placeholder="상세주소"></b-form-input>
+                  </div>
+                </div>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-7" class="form-label text-md-right"><strong>생년 월일</strong></label>
+                <b-form-input class="form-height" v-model="form.birthDate" type="date" id="input-7" ref="birthInput"
+                              placeholder="생년 월일을 입력해주세요" required></b-form-input>
+              </div>
+
+              <div class="mb-4">
+                <label for="input-8" class="form-label text-md-right"><strong>성별</strong></label>
+                <div class="d-flex align-items-center">
+                  <div class="mr-3">
+                    <b-form-radio v-model="form.gender" name="userGender" value="MALE">남성 &nbsp; &nbsp;</b-form-radio>
+                  </div>
+                  <div>
+                    <b-form-radio v-model="form.gender" name="userGender" value="FEMALE">여성</b-form-radio>
+                  </div>
+                </div>
+              </div>
+
+              <br>
+              <b-button @click="onSubmit" class="button-width form-height">수정하기</b-button>
+              <br><br>
+
+            </b-form>
+          </b-card>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {options} from 'axios';
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        confirmPassword: '',
+        name: '',
+        nickname: '',
+        telephone: '',
+        job: '',
+        zoneCode: '',
+        roadAddress: '',
+        detailAddress: '',
+        birthDate: '',
+        gender: 'MALE',
+        role: 'USER',
+      }
+    }
+  },
+  mounted() {
+    this.fnLoginMember();
+  },
+  methods: {
+    options,
+    openPostcode() {
+      new window.daum.Postcode({
+        oncomplete: (data) => {
+          this.form.zoneCode = data.zonecode;
+          this.form.roadAddress = data.roadAddress;
+        },
+      }).open();
+    },
+
+    // 정규 표현식을 사용하여 이메일 유효성 검사
+    isValidEmail(email) {
+      // 간단한 예시이며, 실제로는 복잡한 정규 표현식이 필요할 수 있습니다.
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    },
+
+    emailChkBtn(email) {
+      if (!this.form.email) {
+        alert("이메일을 먼저 입력해주세요.");
+        this.$refs.emailInput.focus();
+        return;
+      }
+      if (!this.isValidEmail(this.form.email)) {
+        alert("올바른 이메일 형식이 아닙니다. 다시 확인해주세요.");
+        this.$refs.emailInput.focus();
+        return;
+      }
+      this.$axios.post(`/api/v1/member/checkEmail/${email}`)
+          .then(() => {
+            console.log(email);
+            alert("사용 가능한 이메일 입니다.");
+            return;
+          }).catch((err) => {
+        alert(err.response.data.message);
+        this.form.email = '';
+        this.$refs.emailInput.focus();
+        this.$store.state.loadingStatus = false;
+        return;
+      });
+    },
+
+    nicknameChkBtn(nickname) {
+      if (!this.form.nickname) {
+        alert("닉네임을 먼저 입력해주세요.");
+        this.$refs.nicknameInput.focus();
+        return;
+      }
+      this.$axios.post(`/api/v1/member/checkNickname/${nickname}`)
+          .then(() => {
+            alert("사용 가능한 닉네임 입니다.");
+            return;
+          }).catch((err) => {
+        alert(err.response.data.message);
+        this.form.nickname = '';
+        this.$refs.nicknameInput.focus();
+        this.$store.state.loadingStatus = false;
+        return;
+      });
+    },
+
+    telephoneChkBtn(telephone) {
+      if (!this.form.telephone) {
+        alert("전화번호를 입력해주세요.");
+        this.$refs.telephoneInput.focus();
+        return;
+      }
+      this.$axios.post(`/api/v1/member/checkTelephone/${telephone}`)
+          .then(() => {
+            alert("사용 가능한 전화번호 입니다.");
+            return;
+          }).catch((err) => {
+        alert(err.response.data.message);
+        this.form.telephone = '';
+        this.$refs.telephoneInput.focus();
+        this.$store.state.loadingStatus = false;
+        return;
+      });
+    },
+
+    fnLoginMember() {
+      this.$axios.post(`/api/v1/member/myInfo`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('user_token')}`
+        }
+      }).then((res) => {
+        console.log(res.data.data);
+        // this.form = res.data.data;
+        this.form.email = res.data.data.email;
+        this.form.name = res.data.data.name;
+        this.form.nickname = res.data.data.nickname;
+        this.form.telephone = res.data.data.telephone;
+        this.form.job = res.data.data.job;
+        this.form.zoneCode = res.data.data.zoneCode;
+        this.form.roadAddress = res.data.data.roadAddress;
+        this.form.detailAddress = res.data.data.detailAddress;
+        this.form.birthDate = res.data.data.birthDate;
+        this.form.address = res.data.data.address;
+        this.form.skillName = res.data.data.skillName;
+        this.form.gender = res.data.data.gender;
+      }).catch((err) => {
+        if (err.response.status === 401 || err.response.status === 404) {
+          this.$router.push({path: '/login'});
+        } else {
+          alert(err.response.data.message);
+        }
+      })
+    },
+
+    onSubmit() {
+      if (!this.form.email) {
+        alert("이메일을 입력해주세요.");
+        this.$refs.emailInput.focus();
+        return;
+      }
+      if (!this.isValidEmail(this.form.email)) {
+        alert("올바른 이메일 형식이 아닙니다. 다시 확인해주세요.");
+        this.$refs.emailInput.focus();
+        return;
+      }
+      if (!this.form.password) {
+        alert("비밀번호를 입력해주세요.");
+        this.$refs.passwordInput.focus();
+        return;
+      }
+      if (!this.form.confirmPassword) {
+        alert("비밀번호 확인을 입력해주세요.");
+        this.$refs.confirmPasswordInput.focus();
+        return;
+      }
+      if (this.form.password != this.form.confirmPassword) {
+        alert("비밀번호가 일치하지 않습니다. 다시 한번 확인해주세요.");
+        this.$refs.confirmPasswordInput.focus();
+        return;
+      }
+      if (!this.form.name) {
+        alert("이름을 입력해주세요.");
+        this.$refs.nameInput.focus();
+        return;
+      }
+      if (!this.form.nickname) {
+        alert("닉네임을 입력해주세요.");
+        this.$refs.nicknameInput.focus();
+        return;
+      }
+      if (!this.form.telephone) {
+        alert("전화번호를 입력해주세요.");
+        this.$refs.telephoneInput.focus();
+        return;
+      }
+      if (!this.form.job) {
+        alert("직업을 입력해주세요.");
+        this.$refs.jobInput.focus();
+        return;
+      }
+      if (!this.form.zoneCode) {
+        alert("우편번호를 입력해주세요.");
+        this.$refs.zoneCodeInput.focus();
+        return;
+      }
+      if (!this.form.roadAddress) {
+        alert("도로명 주소를 입력해주세요.");
+        this.$refs.roadAddressInput.focus();
+        return;
+      }
+      if (!this.form.detailAddress) {
+        alert("상세 주소를 입력해주세요.");
+        this.$refs.detailAddressInput.focus();
+        return;
+      }
+      if (!this.form.birthDate) {
+        alert("생년월일을 선택해주세요.");
+        this.$refs.birthInput.focus();
+        return;
+      }
+
+      this.$axios.post("/api/v1/member/update", this.form, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('user_token')}`
+        }
+      })
+          .then((res) => {
+            console.log(res);
+            alert('회원 정보가 수정 되었습니다.');
+            this.$router.go(-1);
+          }).catch((err) => {
+        alert(err.response.data.message);
+        this.$store.state.loadingStatus = false;
+        return;
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.mb-4 label {
+  text-align: left;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.mb-4 b-input-group {
+  display: flex;
+  align-items: center;
+}
+
+.form-height {
+  height: 50px;
+}
+
+.form-width {
+  width: 80%;
+}
+
+.button-width {
+  width: 100%;
+}
+</style>
---
+++ front/src/views/member/MemberUserView.vue
@@ -0,0 +1,358 @@
+<template>
+
+  <div class="post-list">
+
+    <div class="row mt-5 mb-5">
+      <div class="row mb-3">
+        <h2>회원 정보</h2>
+      </div>
+      <div class="row">
+        <div class="col-2" style="text-align: center">
+          <i class="fa-solid fa-user" style="font-size: 50px"></i>
+        </div>
+        <div class="col">
+          <table>
+            <tr>
+              <td>이름:</td>
+              <td>{{ this.name }}</td>
+            </tr>
+            <tr>
+              <td>닉네임:</td>
+              <td>{{ this.nickname }}</td>
+            </tr>
+            <tr>
+              <td>기술:</td>
+              <td>{{ this.skillName }}</td>
+            </tr>
+            <tr>
+              <td>직업:</td>
+              <td>{{ this.job }}</td>
+            </tr>
+            <tr>
+              <td>주소:</td>
+              <td><small>{{ this.address }}</small></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row post-list mt-5">
+      <div>
+        <!-- 네비게이션 탭 -->
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link hover-pointer" @click="showTab('myPosts')" :class="{ active: activeTab === 'myPosts' }">작성한 게시물</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link hover-pointer" @click="showTab('likedPosts')" :class="{ active: activeTab === 'likedPosts' }">좋아요 누른
+              게시물</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link hover-pointer" @click="showTab('myReplies')" :class="{ active: activeTab === 'myReplies' }">작성한
+              댓글</a>
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="activeTab === 'myPosts'">
+        <div class="mt-3">
+          <div>
+            <div v-for="(item, idx) in postList" :key="idx" @click="fnPostView(item.postId)" class="post-item hover-pointer">
+              <div class="post-category">{{ item.postCategory }} - {{ item.skillName }}</div>
+              <div class="post-title">
+                <span v-if="item.title.length < 20">{{ item.title }} &nbsp;&nbsp;</span>
+                <span v-else>{{ item.title.substring(0, 10) + "..." }}</span>
+              </div>
+              <div class="post-content">
+                {{ truncateAndStripTags(item.content, 100) }}
+              </div>
+
+              <div class="post-status">
+                <i class="fa-solid fa-comment small-icon">{{ item.replyCnt }}&nbsp;</i>
+                <i class="fa-solid fa-thumbs-up small-icon">{{ item.likeCnt }}&nbsp;</i>
+                <i class="fa-solid fa-eye small-icon">{{ item.viewCnt }}&nbsp;</i>
+                <p class="post-reg-date">{{ formatDateTime(item.regDate) }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="activeTab === 'likedPosts'">
+        <div class="mt-3">
+          <div>
+            <div v-for="(item, idx) in likePostList" :key="idx" @click="fnPostView(item.postId)" class="post-item hover-pointer">
+              <div class="post-category">{{ item.postCategory }} - {{ item.skillName }}</div>
+              <div class="post-title">
+                <span v-if="item.title.length < 20">{{ item.title }} &nbsp;&nbsp;</span>
+                <span v-else>{{ item.title.substring(0, 10) + "..." }}</span>
+              </div>
+              <!--            <div class="post-content" v-html="item.content"></div>-->
+              <div class="post-content">
+                {{ truncateAndStripTags(item.content, 100) }}
+              </div>
+
+              <div class="post-status">
+                <i class="fa-solid fa-comment small-icon">{{ item.replyCnt }}&nbsp;</i>
+                <i class="fa-solid fa-heart small-icon">{{ item.likeCnt }}&nbsp;</i>
+                <i class="fa-solid fa-eye small-icon">{{ item.viewCnt }}&nbsp;</i>
+                <p class="post-reg-date">{{ formatDateTime(item.regDate) }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="activeTab === 'myReplies'">
+        <div class="mt-3">
+          <div>
+            <div v-for="(reply, idx) in replyList" :key="idx" @click="fnPostView(reply.postId)" class="reply-container">
+              <div class="reply-detail">
+                <strong class="mouse-cursor">[{{ reply.memberNickname }}]</strong>
+                <br>
+                <span class="small-font">{{reply.memberSkill}}</span>
+                <p class="mt-1">{{ reply.content }}</p>
+              </div>
+              <span><small>{{ formatDateTime(reply.regDate) }} - </small></span>
+              <i class="fa-solid fa-thumbs-up Reply-heart-icon"><small>&nbsp;좋아요 {{reply.likeCnt}}</small></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+
+  data() {
+    return {
+      requestBody: {}, // 리스트 페이지 데이터전송
+      postList: {}, // 내가 작성한 게시물 리스트
+      likePostList: {}, // 내가 좋아요 누른 게시물 리스트
+      replyList: {}, // 내가 작성한 댓글 리스트
+      activeTab: 'myPosts',
+
+      name: '',
+      nickname: '',
+      skillName: '',
+      job: '',
+      address: '',
+
+      postCategory: '',
+      replyCount: '',
+      likeCount: '',
+
+      memberNickname: '',
+    }
+  },
+
+  mounted() {
+    // URL에서 'author' 매개변수 추출
+    const urlParams = new URLSearchParams(window.location.search);
+    this.memberNickname = urlParams.get('author');
+
+    this.fnMember(this.memberNickname);
+    this.fnPostList(this.memberNickname);
+    this.fnLikePostList(this.memberNickname);
+    this.fnReplyList(this.memberNickname);
+  },
+
+  methods: {
+    fnMember(memberNickname) {
+      this.$axios.post(`/api/v1/member/${memberNickname}`).then((res) => {
+        console.log(res);
+
+        this.name = res.data.data.name;
+        this.nickname = res.data.data.nickname;
+        this.skillName = res.data.data.skillName;
+        this.job = res.data.data.job;
+        this.address = res.data.data.roadAddress + " " + res.data.data.detailAddress;
+
+      }).catch((err) => {
+        console.log(err);
+
+        // if (err.response.status === 401 || err.response.status === 400) {
+        //   alert("로그인을 먼저 해주세요!");
+        //   this.$router.push({path: '/login'});
+        // }
+        // if (err.response.status === 404) {
+        //   alert("잘못된 경로입니다.");
+        //   alert(err.response.data.message);
+        //   location.reload()
+        // } else {
+        //   alert(err.response.data.message);
+        //   location.reload()
+        // }
+        // this.$store.state.loadingStatus = false;
+      })
+    },
+    fnPostList(memberNickname) {
+      this.$axios.post(`/api/v1/posts/member/${memberNickname}`).then((res) => {
+            console.log(res);
+            this.postList = res.data.data;
+          }).catch((err) => {
+            console.log(err);
+
+          //   if (err.response.status === 401 || err.response.status === 400) {
+          //     alert("로그인을 먼저 해주세요!");
+          //     this.$router.push({path: '/login'});
+          //   }
+          //   if (err.response.status === 404) {
+          //     alert("잘못된 경로입니다.");
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   } else {
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   }
+          //   this.$store.state.loadingStatus = false;
+          })
+    },
+    fnLikePostList(memberNickname) {
+      this.$axios.post(`/api/v1/posts/like/member/${memberNickname}`).then((res) => {
+            console.log(res);
+            this.likePostList = res.data.data;
+          }).catch((err) => {
+            console.log(err);
+          //   if (err.response.status === 401 || err.response.status === 400) {
+          //     alert("로그인을 먼저 해주세요!");
+          //     this.$router.push({path: '/login'});
+          //   }
+          //   if (err.response.status === 404) {
+          //     alert("잘못된 경로입니다.");
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   } else {
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   }
+          //   this.$store.state.loadingStatus = false;
+          })
+    },
+    fnReplyList(memberNickname) {
+      this.$axios.post(`/api/v1/reply/member/${memberNickname}`).then((res) => {
+            console.log(res);
+
+            this.replyList = res.data.data;
+            console.log(res.data.data);
+          }).catch((err) => {
+            console.log(err);
+
+          //   if (err.response.status === 401 || err.response.status === 400) {
+          //     alert("로그인을 먼저 해주세요!");
+          //     this.$router.push({path: '/login'});
+          //   }
+          //   if (err.response.status === 404) {
+          //     alert("잘못된 경로입니다.");
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   } else {
+          //     alert(err.response.data.message);
+          //     location.reload()
+          //   }
+          //   this.$store.state.loadingStatus = false;
+          })
+    },
+    showTab(tabName) {
+      this.activeTab = tabName;
+    },
+    fnPostView(idx) {
+      this.requestBody.idx = idx
+      this.$router.push({
+        path: '../post/detail',
+        query: {idx}
+      })
+    },
+
+    formatDateTime(dateTimeStr) {
+      const dateTime = new Date(dateTimeStr);
+      const now = new Date();
+
+      const timeDifferenceInSeconds = Math.floor((now - dateTime) / 1000);
+
+      if (timeDifferenceInSeconds < 60) {
+        return `${timeDifferenceInSeconds}초 전`;
+      } else if (timeDifferenceInSeconds < 3600) {
+        const minutes = Math.floor(timeDifferenceInSeconds / 60);
+        return `${minutes}분 전`;
+      } else if (timeDifferenceInSeconds < 86400) {
+        const hours = Math.floor(timeDifferenceInSeconds / 3600);
+        return `${hours}시간 전`;
+      } else {
+        const days = Math.floor(timeDifferenceInSeconds / 86400);
+        return `${days}일 전`;
+      }
+    },
+
+    truncateAndStripTags: function(text, length) {
+      // 태그 제거
+      let strippedText = text.replace(/<[^>]+>/g, '');
+
+      // 길이 제한 및 말줄임표 추가
+      if (strippedText.length <= length) {
+        return strippedText;
+      } else {
+        return strippedText.substring(0, length) + '...';
+      }
+    },
+
+  }
+
+};
+
+</script>
+
+<style scoped>
+.hover-pointer {
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.small-icon {
+  font-size: 15px; /* Adjust the font size to your preference */
+}
+
+.post-item {
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.post-category {
+  color: #888;
+  font-size: 15px;
+  margin-bottom: 5px;
+}
+
+.post-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.post-content {
+  color: #333;
+  margin-bottom: 5px;
+}
+
+.post-status {
+  color: #888;
+}
+
+.post-reg-date {
+  color: #888;
+  text-align: right;
+}
+
+</style>
