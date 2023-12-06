b49d40728b1fbd1a5929b6a46132e3cee6709909
soohykeee <gur1028@skuniv.ac.kr>
2023-12-07 01:10:26 +0900

    feat: pay의 기본 index 페이지

---
+++ front/src/views/pay/PayIndex.vue
@@ -0,0 +1,240 @@
+<template>
+  <div>
+    <h1>결제인증</h1>
+    <b-form @submit.prevent="goPayAuth">
+
+      <div>
+        <!-- container -->
+        <div class="container">
+          <div class="table-responsive">
+            <table class="table">
+              <colgroup>
+                <col style="width: 15%; background-color: lightgray;">
+                <col style="width: 35%;">
+                <col style="width: 15%; background-color: lightgray;">
+                <col style="width: 35%;">
+              </colgroup>
+              <thead>
+              <tr>
+                <th>항목</th>
+                <th>값</th>
+                <th>항목</th>
+                <th>값</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>전문정보</td>
+                <td>
+                  <b-form-input type="text" name="hdInfo" id="hdInfo" placeholder="전문정보"
+                                v-model="form.hdInfo" ref="hdInfoInput" readonly/>
+                </td>
+                <td>전문버전</td>
+                <td>
+                  <b-form-input type="text" name="apiVer" id="apiVer" placeholder="전문버전"
+                                v-model="form.apiVer" ref="apiVerInput" readonly/>
+                </td>
+              </tr>
+              <tr>
+                <td>처리구분</td>
+                <td>
+                  <b-form-input type="text" name="processType" id="processType" placeholder="처리구분"
+                                v-model="form.processType" ref="processTypeInput" readonly/>
+                </td>
+                <td>가맹점아이디</td>
+                <td>
+                  <b-form-input type="text" name="mercntId" id="mercntId" placeholder="가맹점아이디"
+                                v-model="form.mercntId" ref="mercntIdInput" readonly/>
+                </td>
+              </tr>
+              <tr>
+                <td>주문번호</td>
+                <td>
+                  <b-form-input type="text" name="ordNo" id="ordNo" placeholder="주문번호"
+                                v-model="form.ordNo" ref="ordNoInput"/>
+                </td>
+                <td>거래금액</td>
+                <td>
+                  <b-form-input type="text" name="trPricePlain" id="trPricePlain" placeholder="거래금액"
+                                v-model="form.trPricePlain" ref="trPricePlainInput"/>
+                </td>
+              </tr>
+              <tr>
+                <td>상품명</td>
+                <td>
+                  <b-form-input type="text" name="productNm" id="productNm" placeholder="상품명"
+                                v-model="form.productNm" ref="productNmInput"/>
+                </td>
+                <td>면세여부[Y/N/G]</td>
+                <td>
+                  <b-form-input type="text" name="dutyFreeYn" id="dutyFreeYn" placeholder="면세여부"
+                                v-model="form.dutyFreeYn" ref="dutyFreeYnInput" readonly/>
+                </td>
+              </tr>
+              <tr>
+                <td>결과통보URL</td>
+                <td>
+                  <b-form-input type="text" name="callbackUrl" id="callbackUrl" placeholder="결과통보URL"
+                                v-model="form.callbackUrl" ref="callbackUrlInput" readonly/>
+                </td>
+                <td>인증창취소URL</td>
+                <td>
+                  <b-form-input type="text" name="cancelUrl" id="cancelUrl" placeholder="인증창취소URL"
+                                v-model="form.cancelUrl" ref="cancelUrlInput" readonly/>
+                </td>
+              </tr>
+              <tr>
+                <td>가맹점데이터1(선택)</td>
+                <td>
+                  <b-form-input type="text" name="mercntParam1" id="mercntParam1" placeholder="가맹점데이터1"
+                                v-model="form.mercntParam1"/>
+                </td>
+                <td>가맹점데이터2(선택)</td>
+                <td>
+                  <b-form-input type="text" name="mercntParam2" id="mercntParam2" placeholder="가맹점데이터2"
+                                v-model="form.mercntParam2"/>
+                </td>
+              </tr>
+              <tr>
+                <td>viewType[popup/self]</td>
+                <td>
+                  <b-form-input type="text" id="viewType" name="viewType" placeholder="결제창 팝업방식"
+                                v-model="form.viewType" ref="viewTypeInput" readonly/>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <!-- Vue 데이터와 바인딩되는 부분 -->
+        <b-form-input v-model="form.trPrice" name="trPrice" hidden/>
+        <b-form-input v-model="form.trDay" name="trDay" hidden/>
+        <b-form-input v-model="form.trTime" name="trTime" hidden/>
+        <b-form-input v-model="form.signature" name="signature" hidden/>
+
+        <!-- fixed bottom button -->
+        <div class="fix_btm_btn">
+          <button type="button" id="send" class="btn btn_lg" @click="goPayAuth">결제인증요청</button>
+        </div>
+        <!-- //fixed bottom button -->
+      </div>
+
+
+    </b-form>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        hdInfo: 'IA_AUTHPAGE_1.0_1.0',
+        apiVer: '1.0',
+        processType: 'D',
+        mercntId: 'M2266041',
+        ordNo: '',
+        trPricePlain: '',
+        productNm: '',
+        dutyFreeYn: 'N',
+        callbackUrl: window.location.href + '/callback',
+        cancelUrl: window.location.href + '/cancel',
+        mercntParam1: '',
+        mercntParam2: '',
+        viewType: 'self',
+
+        trPrice: "",
+        trDay: "",
+        trTime: "",
+        signature: "",
+      },
+      // 여기에 다른 데이터 추가 가능
+    };
+  },
+  methods: {
+    goPayAuth() {
+      if (!this.form.hdInfo) {
+        alert("전문정보를 입력해주세요.");
+        this.$refs.hdInfoInput.focus();
+        return
+      }
+      if (!this.form.apiVer) {
+        alert("전문버전를 입력해주세요.");
+        this.$refs.apiVerInput.focus();
+        return
+      }
+      if (!this.form.processType) {
+        alert("처리구분을 입력해주세요.");
+        this.$refs.processTypeInput.focus();
+        return
+      }
+      if (!this.form.mercntId) {
+        alert("가맹점 아이디를 입력해주세요.");
+        this.$refs.mercntIdInput.focus();
+        return
+      }
+      if (!this.form.ordNo) {
+        alert("주문 번호를 입력해주세요.");
+        this.$refs.ordNoInput.focus();
+        return
+      }
+      if (!this.form.trPricePlain) {
+        alert("거래 금액을 입력해주세요.");
+        this.$refs.trPricePlainInput.focus();
+        return
+      }
+      if (!this.form.productNm) {
+        alert("상품 명을 입력해주세요.");
+        this.$refs.productNmInput.focus();
+        return
+      }
+      if (!this.form.dutyFreeYn) {
+        alert("면세 여부를 입력해주세요.");
+        this.$refs.dutyFreeYnInput.focus();
+        return
+      }
+      if (!this.form.callbackUrl) {
+        alert("결과 통보 url을 입력해주세요.");
+        this.$refs.callbackUrlInput.focus();
+        return
+      }
+      if (!this.form.cancelUrl) {
+        alert("결과 취소 url을 입력해주세요.");
+        this.$refs.cancelUrlInput.focus();
+        return
+      }
+      if (!this.form.viewType) {
+        alert("결제창 방식을 입력해주세요.");
+        this.$refs.viewTypeInput.focus();
+        return
+      }
+
+      this.$axios.post("/api/v1/payReserv", this.form)
+          .then((res) => {
+            alert(res.data.message)
+
+            // eslint-disable-next-line no-undef
+            SettlePay.pay(obj);
+          }).catch((err) => {
+        alert(err.response.data.message);
+        this.$store.state.loadingStatus = false;
+      });
+
+    },
+
+  },
+};
+</script>
+
+<style>
+b-form-input {
+  width: 280px;
+}
+</style>
+
